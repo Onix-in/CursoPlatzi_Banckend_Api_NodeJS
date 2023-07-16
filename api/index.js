72bb0b203d3fe8bf002baf2576f68d122c79b4aa
Leonardo Rodriguez <jlroca24@yahoo.esz>
2023-07-16 17:05:59 -0500

    las carpetas del proyecto se movieron a una carpeta api, para que cumpla con los parametros de vercel, se modifico el package.json para tener en cuenta la ruta donde esta el index y se modificaron todas las rutas para que contangan el api/

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
+
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); //Usar el Middelware para poder recibir información en formato JSON.
-const whitelist = ['http://localhost:8080', 'http://myapp.co'];
+
+const whitelist = ['http://localhost:8080', 'http://myapp.co']; // las URL autorizadas a conectarse.
 const options = {
   origin: (origin, callback) => {
     if(whitelist.includes(origin) || !origin){
@@ -17,13 +19,13 @@ const options = {
     }
   }
 }
-app.use(cors(options));// habitlia cualquier dominio para que se conecte a la API
+app.use(cors(options));// linea que habilita que dominios se pueden conectar a la API, en la variable options
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola Mi server en Express');
-})
+});
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 })
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,6 @@ const usersRouter = require('./users.router');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
-
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
